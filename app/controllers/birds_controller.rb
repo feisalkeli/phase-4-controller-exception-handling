d8dc3813858a5a844bccad48dc58728e4b1b8e3b
FeisalKeli <kellyfeisal2000@gmail.com>
2023-06-21 17:16:04 +0300

    lab done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -11,6 +11,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
+    
   end
 
   # GET /birds/:id
